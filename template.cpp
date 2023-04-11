9f2148922ea36cc55c77fab356068f1a13dd64cb
koybasimuhittin <koybasimuhittin@gmail.com>
2023-04-12 02:21:56 +0300

    template

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define f1 first
+#define s2 second
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define fri(a) freopen(a,"r",stdin);
+#define fro(a) freopen(a,"w",stdout);
+const int N = 1e6 + 5;
+const int mod = 1e9 + 7;
+
+
+int main( ) {
+
+    fri("in.txt");
+    fro("out.txt");
+
+    return 0;
+}
